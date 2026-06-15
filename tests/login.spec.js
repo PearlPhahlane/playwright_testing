b607b5e3de2cf81075957e9e9e64246722048bbf
Pearl Phahlane <pearlphahlane@Pearls-MacBook-Pro.local>
2026-06-15 16:00:51 +0200

    test correct login credentials and ensure that with incorrect credentials an error is visible

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,22 @@
 const { test, expect } = require ('@playwright/test');
 
-test ('homepage can load', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await expect(page).toHaveTitle(/Swag Labs/i);
+test ('user can login with valid credentials', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/'); //go to login page
+
+
+    await page.locator('[data-test="username"]').fill('standard_user') //enter username
+    await page.locator('[data-test="password"]').fill('secret_sauce'); //enter password
+    await page.locator('[data-test="login-button"]').click(); //click login button
+
+    await expect(page).toHaveURL(/inventory/); //verify user lands on inventory page
 });
+
+test ('user cannot login with invalid credentials', async ( { page } ) =>  {
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.locator('[data-test="username"]').fill('wrong_user');
+    await page.locator('[data-test="password"]').fill('wrong_password');
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page.locator('[data-test="error"]')).toBeVisible();
+})
